b8887095838d951e76832a7aab41ba21be44ed33
Andrew Silva <andrewgameplays6@gmail.com>
2021-12-23 19:18:20 -0300

    init

--- subpage/htmlsubpage/jshtml/index.js
+++ subpage/htmlsubpage/jshtml/index.js
@@ -3,5 +3,18 @@ function minhaFunc() {
 }
 
 function outraFunc() {
-    document.getElementById("demo ").style = "../csshtml/teste.css"
+    const a = document.getElementById("demo ").style;
+    document.getElementById("image").src = "./subpage/imagens/aa.webp"
+    a.fontSize = "40px";
+    a.color = "blue"
 }
+var today = new Date();
+document.getElementById("sa ").innerText = "Horário: " + today.toLocaleTimeString();
+
+function minhaFunca() {
+    var today = new Date();
+    document.getElementById("sa ").innerText = "Horário: " + today.toLocaleTimeString();
+}
+
+
+var t = setInterval(minhaFunca, 1000)
