b7a444e2ffaae97d5de36b7b5c78b8aff7a34527
Andrew Silva <andrewgameplays6@gmail.com>
2021-12-23 17:06:28 -0300

    init

---
+++ subpage/htmlsubpage/jshtml/index.js
@@ -0,0 +1,7 @@
+function minhaFunc() {
+    document.getElementById("demo ").innerHTML = "Paragraph changed.";
+}
+
+function outraFunc() {
+    document.getElementById("demo ").style = "../csshtml/teste.css"
+}
