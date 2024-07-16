dd524aeb1cd81c3ace9b0b4f07e353eb239babae
AndrewHTR <andrewgameplays6@gmail.com>
2024-07-16 17:48:03 -0300

    finalized css and html

--- modules/loader.js
+++ modules/loader.js
@@ -1,16 +1,6 @@
-document.getElementById("navizinha").innerHTML =
-        '<a href="/index.html">Home</a>' +
-        '<a href="/projects/projects.html">Projects</a>' +
-        '<a href="sobre.html">About</a>';
 
-document.getElementById('footinho').innerHTML = 
-        '<a href="https://github.com/AndrewHTR">' +
-            '<img id="imagem" src="https://github.com/AndrewHTR.png" alt="Imagem">' +
-        '</a>' +
-        '<div id="imagem-div">' +
-            '<h2 class="fot">Andrew Silva</h2>' +
-            "<p class='fot'>I love overgeared, but i don't like fish.</p>" +
-        '</div>';
+
+
 
 /*
 window.onscroll = function() {myFunction()}; 
